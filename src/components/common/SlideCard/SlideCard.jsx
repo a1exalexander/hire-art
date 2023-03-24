3da3798802c649f24465b13c9a26d33613afca5c
ANDREIEV VIACHESLAV <andreiev.viacheslav@gmail.com>
2023-03-24 13:35:37 +0200

    ✅ add SlideCard props

--- src/components/common/SlideCard/SlideCard.jsx
+++ src/components/common/SlideCard/SlideCard.jsx
@@ -1,3 +1,26 @@
-export const SlideCard = () => {
-  return <div>SlideCard</div>;
+import {
+  StyledSlideCard,
+  Title,
+  Description,
+  Name,
+  Position,
+  Img,
+  Wrapper,
+  Inner,
+} from "./SlideCard.styled";
+
+export const SlideCard = ({ title, desc, name, pos, img }) => {
+  return (
+    <StyledSlideCard>
+      <Title>{title}</Title>
+      <Description>{desc}</Description>
+      <Wrapper>
+        <Img src={img} alt={name} />
+        <Inner>
+          <Name>{name}</Name>
+          <Position>{pos}</Position>
+        </Inner>
+      </Wrapper>
+    </StyledSlideCard>
+  );
 };
--- src/components/common/SlideCard/SlideCard.styled.jsx
+++ src/components/common/SlideCard/SlideCard.styled.jsx
@@ -1,2 +1,51 @@
 import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
+
+export const StyledSlideCard = styled.div`
+  max-width: 420px;
+  width: 100%;
+  height: 100%;
+  background: #ffffff;
+  border: 1px solid #c2cadf;
+  border-radius: 12px;
+  padding: 40px;
+`;
+
+export const Title = styled.p`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 170%;
+  color: #0f1723;
+`;
+export const Description = styled.p`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 170%;
+  color: #4b5565;
+  margin-bottom: 83px;
+`;
+export const Name = styled.p`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 27px;
+  color: #0f1723;
+`;
+export const Position = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 140%;
+  color: #697586;
+`;
+export const Img = styled.img`
+  width: 64px;
+  height: 64px;
+  margin-right: 16px;
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
