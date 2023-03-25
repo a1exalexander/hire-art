93a2ff5021251fbcdf79349cd33601468185069a
ANDREIEV VIACHESLAV <andreiev.viacheslav@gmail.com>
2023-03-25 14:08:33 +0200

    ✅ add CustomersSay

--- src/components/common/Slider/Slider.jsx
+++ src/components/common/Slider/Slider.jsx
@@ -1,50 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import { Navigation } from "swiper";
-
 import "swiper/css";
-
 import { SlideCard } from "../SlideCard/SlideCard";
 import "./Slider.css";
-
-import photo_1 from "./SliderImg/photo_1.svg";
-import photo_2 from "./SliderImg/photo_2.svg";
-import photo_3 from "./SliderImg/photo_3.svg";
-
-const slides = [
-  {
-    id: 1,
-    name: "Cory Delmeir",
-    title: "The best ones!",
-    desc: "I was using a couple of services, but only HireArt really helped… They have great features like intuitive UI, ease of use /UX and great customer service agents like Anna, who helped through the process. I will recommend it to my friends.  Give this service a chance!",
-    img: photo_1,
-    pos: "Frontend developer",
-  },
-  {
-    id: 2,
-    name: "Cory Delmeir",
-    title: "Excellent service for both candidates & HRs",
-    desc: "I had over 60 open positions in 2022 so I had been dreading the hiring flow, like how I would go through everything and figure out the best ones. Fortunately, I found HireArt and it's honestly saved me so much time. I am a new one in the field, but Jon from support helped me out right away. I'm very happy with this platform and will definitely use it again for my further carrier.",
-    img: photo_2,
-    pos: "HR at Netflix",
-  },
-  {
-    id: 3,
-    name: "Cory Delmeir",
-    title: "Works for both!",
-    desc: "HireArt is one of the few companies in the industry that has good spaces for both Hr specialists and artistic minds. This service respects users and saves time. Their interface is also very intuitive to use and does an awesome job of simplifying a very complex applying and hiring profile. I'm extremely happy with HireArt. ",
-    img: photo_3,
-    pos: "Visual Creator",
-  },
-  {
-    id: 4,
-    name: "Cory Delmeir",
-    title: "Works for both!",
-    desc: "HireArt is one of the few companies in the industry that has good spaces for both Hr specialists and artistic minds. This service respects users and saves time. Their interface is also very intuitive to use and does an awesome job of simplifying a very complex applying and hiring profile. I'm extremely happy with HireArt. ",
-    img: photo_3,
-    pos: "Visual Creator",
-  },
-];
 
 const Arrow = ({ className }) => {
   return (
@@ -62,7 +20,7 @@ const Arrow = ({ className }) => {
   );
 };
 
-export const Slider = () => {
+export const Slider = ({ slides = [] }) => {
   return (
     <>
       <Swiper
---
+++ src/components/website/CustomersSay/CustomersSay.styled.jsx
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
+
+export const Title = styled.h1`
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 48px;
+  text-align: center;
+  color: #0f1723;
+  margin-button: 64px;
+`;
