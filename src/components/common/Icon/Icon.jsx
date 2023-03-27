8d667de0c8131a2d082d06e641672749e2b0633b
INNA SHUTKA <inna102992@gmail.com>
2023-03-26 22:30:50 -0500

    add accordion

--- src/components/common/Icon/Icon.jsx
+++ src/components/common/Icon/Icon.jsx
@@ -1,8 +1,10 @@
-import PropTypes from 'prop-types';
+import PropTypes, { string } from 'prop-types';
 
 export const ICON_TYPES = {
   filter: 'filter',
   piggyBank: 'piggyBank',
+  plus: 'plus',
+  minus: 'minus',
 };
 
 export const Icon = ({ className, name, size, color }) => {
@@ -33,12 +35,35 @@ export const Icon = ({ className, name, size, color }) => {
           />
         </svg>
       );
+    case ICON_TYPES.plus:
+      return (
+        <svg {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path 
+            fillRule="evenodd" 
+            clipRule="evenodd" 
+            d="M12 5.25C12.4142 5.25 12.75 5.58579 12.75 6V11.25H18C18.4142 11.25 18.75 11.5858 18.75 12C18.75 12.4142 18.4142 12.75 18 12.75H12.75V18C12.75 18.4142 12.4142 18.75 12 18.75C11.5858 18.75 11.25 18.4142 11.25 18V12.75H6C5.58579 12.75 5.25 12.4142 5.25 12C5.25 11.5858 5.58579 11.25 6 11.25H11.25V6C11.25 5.58579 11.5858 5.25 12 5.25Z"
+          />
+        </svg>
+      );
+    case ICON_TYPES.minus:
+      return (
+        <svg {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path 
+            d="M6 12H18" 
+            stroke="#5073EE" 
+            strokeWidth="1.5" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+          />
+        </svg>
+      );
     default:
       return null;
   }
 };
 
 Icon.propTypes = {
+  className: PropTypes.string,
   name: PropTypes.oneOf(Object.values(ICON_TYPES)).isRequired,
   size: PropTypes.number,
   color: PropTypes.string,
