b2bb1a910dde8e7d2e142f2a399f3992e828d9b8
Vlad Konoplyov <vladyslav.konoplov@gmail.com>
2023-03-27 13:01:31 +0300

    "Comment" component added

--- src/components/website/Comment/Services.jsx
+++ src/components/website/Comment/Services.jsx
@@ -1,3 +1,9 @@
+import { Section, Quote, Highlight, Quoter } from "./Services.styled";
 export const Comment = () => {
-  return <div>Comment</div>;
+  return (
+    <Section>
+        <Quote>"Choose a <Highlight>job you love</Highlight>, and you will never have to work a day in your life."</Quote>
+        <Quoter>Winston Churchill</Quoter>
+    </Section>
+  );
 };
